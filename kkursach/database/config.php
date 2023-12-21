01f1623da3a798d46d14a0ebc4fa8e7cecafd246
DarkAugustt <115950264+DarkAugustt@users.noreply.github.com>
2023-12-21 13:48:54 +0300

    Delete kkursach directory

--- kkursach/database/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Конфиг для настройки доступа к базе данных и бота Telegram для авторизации
-$BOT_TOKEN = "6788453230:AAE4X9ZBNQ1d9ZjSRV7VzfIxuzCgEX4x1-g";
-$BOT_USERNAME = "ZZDorogi_bot";
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
-$dbname = "zzd";
